a250f16a213db92a47f5cf23afa729f6aa53c5d9
Arun Giri <37473454+17BIT0216@users.noreply.github.com>
2024-01-30 12:41:01 +0530

    Revert "pushing in doc changes"
    This reverts commit 88e8fc3901c0ff491ed54c4d16e3dbbfbbfd5398.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import './App.css';
 import NavigationBar from './NavigationBar';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Import Navigate
 import BusLayout from './BusLayout';
 import Dashboard from './DashBoard';
 
@@ -14,9 +14,9 @@ const App = () => {
       <div style={styles}>
         <NavigationBar />
         <Routes>
-          <Route path="/" element={<Navigate to="reservation" />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/reservation" element={<BusLayout />} />
+          <Route path="/my-app" element={<Navigate to="/my-app/reservation" />} /> {/* Add a route to redirect to /reservation */}
+          <Route path="/my-app/dashboard" element={<Dashboard />} />
+          <Route path="/my-app/reservation" element={<BusLayout />} />
         </Routes>
       </div>
     </Router>
