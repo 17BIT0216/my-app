09973db8e31eacb1c7af669bc2b670f4668c51d8
Arun Giri <37473454+17BIT0216@users.noreply.github.com>
2024-01-30 11:45:02 +0530

    updating paths as per requirment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import NavigationBar from './NavigationBar';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Import Navigate
 import BusLayout from './BusLayout';
 import Dashboard from './DashBoard';
 
@@ -10,15 +10,16 @@ const styles = {
 
 const App = () => {
   return (
-      <Router>
-          <div style={styles}>
-              <NavigationBar />
-              <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/reservation" element={<BusLayout />} />
-                </Routes>
-          </div>
-      </Router>
+    <Router>
+      <div style={styles}>
+        <NavigationBar />
+        <Routes>
+          <Route path="/my-app" element={<Navigate to="/my-app/reservation" />} /> {/* Add a route to redirect to /reservation */}
+          <Route path="/my-app/dashboard" element={<Dashboard />} />
+          <Route path="/my-app/reservation" element={<BusLayout />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
